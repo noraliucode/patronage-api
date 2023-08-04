3fb02d5bfc3b35987e8eb82fa376394daa99599c
Nora Liu <noraliu0830@gmail.com>
2023-08-04 16:54:17 +0800

    feat: add JWT for authentication

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import fs from "fs";
 import bodyParser from "body-parser";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+const SECRET_KEY = process.env.SECRET_KEY || "";
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,7 +19,7 @@ function readJSONFile(): any[] {
   }
 }
 
-function writeJSONFile(data: any[]): void {
+async function writeJSONFile(data: any[]): Promise<void> {
   try {
     fs.writeFileSync("src/data.json", JSON.stringify(data));
   } catch (err) {
@@ -27,9 +31,24 @@ app.get("/data", (req: Request, res: Response) => {
   res.json(readJSONFile());
 });
 
-app.post("/data", (req: Request, res: Response) => {
-  writeJSONFile(req.body);
-  res.status(201).send();
+app.post("/data", async (req: Request, res: Response) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (token) {
+    try {
+      jwt.verify(token, SECRET_KEY);
+
+      // Write request body to the JSON file
+      await writeJSONFile(req.body);
+
+      res.status(200).send({ message: "Data written to file" });
+    } catch (err) {
+      // if the token has expired or has any other error, return 401
+      res.status(401).send({ error: "Unauthorized" });
+    }
+  } else {
+    // if no token present, return 401
+    res.status(401).send({ error: "No token provided" });
+  }
 });
 
 const port = 3000;
