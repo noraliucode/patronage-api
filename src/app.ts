592e20c5b503827496d5cf92fab9fa929fad5861
Nora Liu <noraliu0830@gmail.com>
2023-08-28 18:16:20 +0800

    feat: add creators CRUD

--- src/app.ts
+++ src/app.ts
@@ -104,18 +104,82 @@ app.patch("/announced", async (req: Request, res: Response) => {
   }
 });
 
-// TODO: comment out this route for now
-// app.get("/data/:id", (req, res) => {
-//   if (ObjectId.isValid(req.params.id)) {
-//     db.collection("data")
-//       .findOne({ _id: new ObjectId(req.params.id) })
-//       .then((doc: any) => {
-//         res.status(200).json(doc);
-//       })
-//       .catch((err: any) => {
-//         res.status(500).json({ error: "Could not fetch the document" });
-//       });
-//   } else {
-//     res.status(500).json({ error: "Could not fetch the document" });
-//   }
-// });
+app.get("/creators/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("creators")
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then((doc: any) => {
+        res.status(200).json(doc);
+      })
+      .catch((err: any) => {
+        res.status(500).json({ error: "Could not fetch the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Could not fetch the document" });
+  }
+});
+
+app.patch("/creators/:id", async (req: Request, res: Response) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+  if (!ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ error: "Invalid ObjectId" });
+  }
+
+  try {
+    jwt.verify(token, SECRET_KEY);
+
+    const result = await db
+      .collection("creators")
+      .updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+app.post("/creators", async (req: Request, res: Response) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+
+  try {
+    jwt.verify(token, SECRET_KEY);
+
+    const newCreator = req.body;
+    const result = await db.collection("creators").insertOne(newCreator);
+
+    res.status(201).json({ result });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+app.delete("/creators/:id", async (req: Request, res: Response) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+  if (!ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ error: "Invalid ObjectId" });
+  }
+
+  try {
+    jwt.verify(token, SECRET_KEY);
+
+    const result = await db
+      .collection("creators")
+      .deleteOne({ _id: new ObjectId(req.params.id) });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: "Creator not found" });
+    }
+
+    res.status(200).json({ message: "Creator deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
